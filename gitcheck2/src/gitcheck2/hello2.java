5374226e31e416986b10cff751ce3150ddaa87fe
NitinChoudhari <66218606+NitinChoudhari@users.noreply.github.com>
2022-07-14 15:16:53 +0530

    Update hello2.java

--- gitcheck2/src/gitcheck2/hello2.java
+++ gitcheck2/src/gitcheck2/hello2.java
@@ -3,7 +3,8 @@ package gitcheck2;
 public class hello2 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!!");
+		System.out.println("Hello I am Nitin Bro!!");
+		// I update the code...
 	}
 
 }

8ce622e0ac1df8c94a533432f724d5df99385301
Suraj Kumar Yadav <56271380+srjsurajydv@users.noreply.github.com>
2022-07-15 12:20:58 +0530

    Update hello2.java

--- gitcheck2/src/gitcheck2/hello2.java
+++ gitcheck2/src/gitcheck2/hello2.java
@@ -3,7 +3,7 @@ package gitcheck2;
 public class hello2 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello I am Nitin Bro!!");
+		System.out.println("Hello I am Nitin Brooooo!!");
 		// I update the code...
 	}
 
